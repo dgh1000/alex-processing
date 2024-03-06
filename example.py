638f112daf9f577caa04ebf096be367bdec0e485
Mike Mossey <michaelmossey@gmail.com>
2024-03-06 14:01:17 -0800

    example of functions

---
+++ example.py
@@ -0,0 +1,15 @@
+def print_message():
+    print("hello Dave. This is HAL 9000.")
+
+def process_numbers(x, y):
+    z = x * 10 + y
+    return z
+
+def main():
+    print_message()
+    num1 = process_numbers(3, 5)
+    print(num1)
+    num2 = process_numbers(1, 2)
+    print(num2)
+    print_message()
+    print_message()

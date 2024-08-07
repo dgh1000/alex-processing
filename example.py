0ae8e9b37de334aa6cec0dd9d6ed300ffc3c6e6e
Alexander Y <alexanderthegreatlegend@gmail.com>
2024-08-07 16:40:43 -0700

    work

--- example.py
+++ example.py
@@ -13,3 +13,31 @@ def main():
     print(num2)
     print_message()
     print_message()
+
+# building: build bottom to top
+# outline : generally build top-down maybe never sure what the bottom is.
+# Module top-level code.
+# Module is a Python file. 
+# at top level we use numbers 1, 2, 3, ...
+# at second level we use letters A, B, C, ...
+# at third level we use roman numerals (lower case)
+# 1. burger flipping simulator - level 1 headings
+#   A. you flip burgers
+#       i. take your spatula and flip the burger on the stove til both sides are cooked
+#       ii. set stove temperature
+#       iii. clean stove top
+#   B. you serve customers
+#   c. you fire your employees
+# 2. hiking
+#   A. take a hike around the map
+# 3. GTA 6
+#   A. you kill people
+#   B. you rob banks
+# 1. 2. 3. are at the top-level 
+print_message()
+for i in range(5):
+    print_message()
+print("i've been trying to reach you for months now")
+print_message()
+print("everything has changed since i last saw you")
+print_message()
---
+++ somethingsomething.py
@@ -0,0 +1,21 @@
+# house = "structure"
+# print(house + house)
+# rock = 5
+# rock_two = 4
+# print(rock_two * 2.5)
+
+number = 10
+number2 = 2
+print(number + number2)
+my_variable = "thing"
+var2 = my_variable
+print (my_variable)
+
+
+
+# int + int is an int
+# int + float is a float
+# float + int 
+# int * int is an int
+# int * float: float
+# float * int : float
